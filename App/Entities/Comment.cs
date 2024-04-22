93ddd8894f5300858438c9578204badcba42b6f2
FabioPenedo <fabiopenedo21@gmail.com>
2024-04-22 11:13:02 -0300

    primeiro commit, exercicio 2

---
+++ App/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; } = string.Empty;
+
+        public Comment() { }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,35 @@
+﻿using App.Entities;
+using System;
+using System.Globalization;
+
+namespace App // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new("Have a nice trip!");
+            Comment c2 = new("Wow that's awesome!");
+            Post p1 = new(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new("Good night");
+            Comment c4 = new("May the Force be with you");
+            Post p2 = new(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
